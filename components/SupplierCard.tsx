a62c0d5fc4db559ead422a4b6aec00c605ec04cd
cenael <rachaelcensuales@gmail.com>
2026-05-06 18:04:47 +0200

    changed path from it to eng

--- components/SupplierCard.tsx
+++ components/SupplierCard.tsx
@@ -109,7 +109,7 @@ export default function FornitoreCard({
                 </div>
                 <div className="col-span-3 flex gap-3 justify-end">
                   <button
-                    onClick={() => router.push(`/modifica-fattura/${invoice.id}`)}
+                    onClick={() => router.push(`/edit-invoice/${invoice.id}`)}
                     className="px-2 py-1 text-slate-500 hover:text-slate-700 transition-colors text-sm"
                     title="Modifica"
                   >
